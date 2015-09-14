725ee1f4d2b13b73c7ed40a485bc73e7820ba3a4
Romain Séguy <rom.seguy@gmail.com>
2015-09-14 09:16:16 +0200

    Implement visualizer

--- containers/App.js
+++ containers/App.js
@@ -1,3 +1,4 @@
+import visualizer from 'd3-state-visualizer';
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -7,11 +8,22 @@ import * as TodoActions from '../actions/todos';
 
 class App extends Component {
   render() {
-    const { todos, dispatch } = this.props;
+    const { state, todos, dispatch } = this.props;
     const actions = bindActionCreators(TodoActions, dispatch);
+    const { components: { TreeChart } } = visualizer;
 
     return (
       <div>
+        <TreeChart
+          state={state}
+          id='todosState'
+          aspectRatio={0.5}
+          isSorted={false}
+          heightBetweenNodesCoeff={1}
+          widthBetweenNodesCoeff={1.5}
+          style={{ 'border-bottom': '1px solid #ededed', width: 'auto' }}
+          tooltipOptions={{ offset: {left: 30, top: 10}, indentationSize: 2}}
+          />
         <Header addTodo={actions.addTodo} />
         <MainSection todos={todos} actions={actions} />
       </div>
@@ -26,6 +38,7 @@ App.propTypes = {
 
 function mapStateToProps(state) {
   return {
+    state,
     todos: state.todos
   };
 }
