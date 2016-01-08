d7b3de70b7729a35afc0ad26c3323738625cef98
Romain Séguy <rom.seguy@gmail.com>
2016-01-08 11:18:29 +0100

    import new todomvc

--- containers/App.js
+++ containers/App.js
@@ -1,46 +1,40 @@
-import visualizer from 'd3-state-visualizer';
-import React, { Component, PropTypes } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import Header from '../components/Header';
-import MainSection from '../components/MainSection';
-import * as TodoActions from '../actions/todos';
+import React, { Component, PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import Header from '../components/Header'
+import MainSection from '../components/MainSection'
+import * as TodoActions from '../actions/todos'
 
 class App extends Component {
   render() {
-    const { state, todos, dispatch } = this.props;
-    const actions = bindActionCreators(TodoActions, dispatch);
-    const { components: { TreeChart } } = visualizer;
-
+    const { todos, actions } = this.props
     return (
       <div>
-        <TreeChart
-          state={state}
-          id='todosState'
-          aspectRatio={0.5}
-          isSorted={false}
-          heightBetweenNodesCoeff={1}
-          widthBetweenNodesCoeff={1.5}
-          style={{ 'border-bottom': '1px solid #ededed', width: 'auto' }}
-          tooltipOptions={{ offset: {left: 30, top: 10}, indentationSize: 2}}
-          />
         <Header addTodo={actions.addTodo} />
         <MainSection todos={todos} actions={actions} />
       </div>
-    );
+    )
   }
 }
 
 App.propTypes = {
   todos: PropTypes.array.isRequired,
-  dispatch: PropTypes.func.isRequired
-};
+  actions: PropTypes.object.isRequired
+}
 
 function mapStateToProps(state) {
   return {
-    state,
     todos: state.todos
-  };
+  }
 }
 
-export default connect(mapStateToProps)(App);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(TodoActions, dispatch)
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
+    'webpack-hot-middleware/client',
     './index'
   ],
   output: {
@@ -14,35 +13,37 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
   ],
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: [ 'babel' ],
       exclude: /node_modules/,
       include: __dirname
     }, {
       test: /\.css?$/,
-      loaders: ['style', 'raw'],
+      loaders: [ 'style', 'raw' ],
       include: __dirname
     }]
   }
-};
+}
 
 
 // When inside Redux repo, prefer src to compiled version.
 // You can safely delete these lines in your project.
-var reduxSrc = path.join(__dirname, '..', '..', 'src');
-var reduxNodeModules = path.join(__dirname, '..', '..', 'node_modules');
-var fs = require('fs');
+var reduxSrc = path.join(__dirname, '..', '..', 'src')
+var reduxNodeModules = path.join(__dirname, '..', '..', 'node_modules')
+var fs = require('fs')
 if (fs.existsSync(reduxSrc) && fs.existsSync(reduxNodeModules)) {
   // Resolve Redux to source
-  module.exports.resolve = { alias: { 'redux': reduxSrc } };
+  module.exports.resolve = { alias: { 'redux': reduxSrc } }
   // Compile Redux from source
   module.exports.module.loaders.push({
     test: /\.js$/,
-    loaders: ['babel'],
+    loaders: [ 'babel' ],
     include: reduxSrc
-  });
+  })
 }
