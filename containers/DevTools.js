fbc9faecd4d3ebba73c78b89abec9ac146a491ca
Romain Séguy <rom.seguy@gmail.com>
2016-01-10 10:26:02 +0100

    Add some tooltipOptions

--- containers/DevTools.js
+++ containers/DevTools.js
@@ -5,13 +5,28 @@ import { createDevTools } from 'redux-devtools';
 import ChartMonitor from 'redux-devtools-chart-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 
+const tooltipOptions = {
+    disabled: false,
+    offset: {left: 30, top: 10},
+    indentationSize: 2,
+    style: {
+      'background-color': 'lightgrey',
+      'opacity': '0.7',
+      'border-radius': '5px',
+      'padding': '5px'
+    }
+}
+
 const DevTools = createDevTools(
   <DockMonitor
     toggleVisibilityKey="ctrl-h"
     changePositionKey="ctrl-q"
     defaultIsVisible={true}
   >
-    <ChartMonitor theme='tomorrow'/>
+    <ChartMonitor
+     theme='tomorrow'
+     tooltipOptions={tooltipOptions}
+    />
   </DockMonitor>
 );
 
